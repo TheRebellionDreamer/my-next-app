81ed6be12edca318245b42c5251b16948e554063
TheRebellionDreamer <rebellion.dreamer@yandex.ru>
2022-07-01 16:50:55 +0300

    Fix webpack configuration

--- next.config.js
+++ next.config.js
@@ -1,15 +1,28 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
   webpack(config, options) {
     config.module.rules.push({
       loader: '@svgr/webpack',
+      issuer: /\.(js|ts)x?$/,
+      options: {
+        prettier: false,
+        svgo: true,
+        svgoConfig: {
+          plugins: [
+            {
+              name: 'preset-default',
+              params: {
+                override: {
+                  removeViewBox: false,
+                },
+              },
+            },
+          ],
+        },
+        titleProp: true,
+      },
       test: /\.svg$/,
     });
-
     return config;
   },
 };
-
-// eslint-disable-next-line no-undef
-module.exports = nextConfig;
