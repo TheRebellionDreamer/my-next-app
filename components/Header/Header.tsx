6e09690dcb3804f8f0c6efe99a4600473bd7bb04
TheRebellionDreamer <rebellion.dreamer@yandex.ru>
2022-07-01 14:25:19 +0300

    Fixed names

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,17 +1,17 @@
-import { HtagProps } from './Header.props';
-import styles from './Htag.module.css';
+import { HeaderProps } from './Header.props';
+import styles from './Header.module.css';
 
-function Htag({ tag, children }: HtagProps): JSX.Element {
+function Header({ tag, children }: HeaderProps): JSX.Element {
   switch (tag) {
     case 'h1':
       return <h1 className={styles.h1}>{children}</h1>;
     case 'h2':
-      return <h2 className={styles.header}>{children}</h2>;
+      return <h2 className={styles.h2}>{children}</h2>;
     case 'h3':
-      return <h3 className={styles.header}>{children}</h3>;
+      return <h3 className={styles.h3}>{children}</h3>;
     default:
       return <></>;
   }
 }
 
-export default Htag;
+export default Header;
