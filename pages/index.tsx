9203d7dc0b92d891bcb6df09d7694711ad8b81b4
TheRebellionDreamer <rebellion.dreamer@yandex.ru>
2022-07-04 12:02:24 +0300

    Install axios, create static props from page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
+import axios from 'axios';
+import { GetStaticProps } from 'next';
 import { useState } from 'react';
 import { Paragraph, Rating } from '../components';
+import { MenuItem } from '../interfaces/menu.interface';
 import { withLayout } from '../layout';
 
-function Home(): JSX.Element {
+function Home({ menu, firstCategory }: HomeProps): JSX.Element {
   const [rating, setRating] = useState<number>(1);
   return (
     <>
@@ -13,8 +16,32 @@ function Home(): JSX.Element {
         nostrud.
       </Paragraph>
       <Rating rating={rating} isEditable onChangeRating={setRating} />
+      <ul>
+        {menu.map((item) => (
+          <li key={item._id.secondCategory}>{item._id.secondCategory}</li>
+        ))}
+      </ul>
     </>
   );
 }
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem[]>(
+    process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find',
+    { firstCategory },
+  );
+  return {
+    props: {
+      menu,
+      firstCategory,
+    },
+  };
+};
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[];
+  firstCategory: number;
+}
